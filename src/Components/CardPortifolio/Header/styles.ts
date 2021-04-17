31c28a2297cbb54f91bfd873f58364b0cf96776e
Rogerio Orioli <rogerio@designhope.com.br>
2021-04-17 19:40:03 -0300

    add blog

--- src/Components/CardPortifolio/Header/styles.ts
+++ src/Components/CardPortifolio/Header/styles.ts
@@ -10,25 +10,48 @@ export const HeaderContainer = styled.div`
         align-items: center;
         z-index: 9999;
         position : relative;
-
-        h1 {
-            a{
-                display : flex;
-                align-items: center;
-                font-size: 3rem;
-                text-decoration: none;
-                position: relative;
-                img {
-                    width : 66px;
-                    height : 66px;
-                       object-fit: cover;
-                    margin : 0 1.6rem;
-                    border-radius: 50%;
-                    border : 2px solid var(--purple);
-                    transition : all 0.2s;
+        .header-content {
+            display : flex;
+            align-items : center;
+            justify-content : space-between;
+            h1 {
+                a{
+                    display : flex;
+                    align-items: center;
+                    font-size: 3rem;
+                    text-decoration: none;
+                    position: relative;
+                    img {
+                        width : 66px;
+                        height : 66px;
+                        object-fit: cover;
+                        margin : 0 1.6rem;
+                        border-radius: 50%;
+                        border : 2px solid var(--purple);
+                        transition : all 0.2s;
+                    }
+                    &:hover {
+                        opacity : .9;
+                    }
                 }
-                &:hover {
-                    opacity : .9;
+            }
+            nav {
+                ul {
+                    li {
+                        display : inline-block;
+                        margin : 0 0  0 20px;
+                        font-size :3rem;
+                        font-weight : bold;
+                        a {
+                         text-decoration : none;
+                        }
+                        &.active{
+                            border-bottom: 3px solid  var(--purple);
+                            a {
+                                
+                            }
+                        }
+                    }
                 }
             }
         }
---
+++ src/pages/articles.tsx
@@ -0,0 +1,55 @@
+import Axios from 'axios';
+import { Head } from 'next/document';
+import React, { useEffect, useState } from 'react';
+import { FiGithub, FiLinkedin, FiTwitter, FiCodepen, FiPhone, FiDownload } from 'react-icons/fi';
+import { ImNewspaper } from 'react-icons/im';
+import Card from '../Components/Card';
+import Hero from '../Components/Hero';
+import SocialIcons from '../Components/SocialIcons';
+import Title from '../Components/Title';
+import Layouts from '../Layouts';
+import PageContainer from './style';
+
+
+// import { Container } from './styles';
+
+const articles: React.FC = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    Axios.get('https://dev.to/api/articles?username=rogeriorioli')
+      .then(success => {
+        const { data } = success
+        setPosts(data)
+      })
+
+  }, [])
+  return (
+    <>
+      <Layouts>
+        <div className="container">
+          <Title contenTitle=" Blog Posts from Dev.to" >
+            <ImNewspaper size={28} />
+          </Title>
+          {posts.map(post =>
+            <Card
+              key={post.id}
+              title={post.title}
+              slug={`articles/${post.slug}`}
+              link={`articles/${post.id}`}
+              alt={post.title}
+              description={post.description}
+              label={post.tags}
+              userAvatar={post?.user.profile_image}
+              username={post.user.username}
+            />
+
+          )}
+        </div>
+      </Layouts>
+    </>
+  );
+}
+
+export default articles;
+
